3fabcd0f1be36c64ab96f4bf58cebd4d7912ade6
frankrep <129170114+frankrep@users.noreply.github.com>
2024-05-13 17:12:44 +0200

    Creata la enumerazione Color

---
+++ src/main/java/it/uniba/app/Color.java
@@ -0,0 +1,8 @@
+package it.uniba.app;
+
+/**
+ * Color è una enumerazione per la gestione dei colori. In questa enumerazione sono riportati tutti i colori utilizzati nel programma.
+ */
+public enum Color {
+
+}
