33eed1ceb6cfedafdd945b6072e0007c3e13083a
ninofrasca <162120477+ninofrasca@users.noreply.github.com>
2024-05-15 18:10:55 +0200

    Aggiunte segnature dei metodi della classe Output e relativi commenti

--- src/main/java/it/uniba/app/Output.java
+++ src/main/java/it/uniba/app/Output.java
@@ -5,5 +5,18 @@ package it.uniba.app;
  * <p>Questa classe gestisce la stampa degli elementi del gioco Ataxx, come la stampa del campo da gioco e dell'interfaccia grafica.</p>
  */
 public class Output {
+    /**
+     * Metodo che mostra a video lo stato del campo in input
+     * @param f campo da mostrare a video
+     */
+    void printField(Field f){
 
+    }
+
+    /**
+     * Metodo che mostra a video il campo vuoto su cui giocare
+     */
+    void printEmptyField(){
+
+    }
 }
