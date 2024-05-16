0c43e2c13b76ebb075007f8d50d3d4a758b0cf7a
ninofrasca <162120477+ninofrasca@users.noreply.github.com>
2024-05-16 16:11:06 +0200

    Rimosso metodo printField e sistemato checkstyle della classe Output.

--- src/main/java/it/uniba/app/Output.java
+++ src/main/java/it/uniba/app/Output.java
@@ -2,36 +2,41 @@ package it.uniba.app;
 
 /**
  * Output è la classe che si occupa delle stampe a video.
- * <p>Questa classe gestisce la stampa degli elementi del gioco Ataxx, come la stampa del campo da gioco e dell'interfaccia grafica.</p>
+ * <p>Questa classe gestisce la stampa degli elementi del gioco Ataxx,
+ * come la stampa del campo da gioco e dell'interfaccia grafica.</p>
  */
-public class Output {
+public final class Output {
     /**
-     * Metodo che mostra a video il campo vuoto su cui giocare
+     * Costruttore per la classe.
      */
-    static void printEmptyField(){
+    private Output() { }
+    /**
+     * Metodo che mostra a video il campo vuoto su cui giocare.
+     */
+    static void printEmptyField() {
         final int dim = 7;
         System.out.print("   ");
-        for(int i=1;i<=dim;i++){
+        for (int i = 1; i <= dim; i++) {
             System.out.print("____ ");
         }
         System.out.print("\n");
         System.out.print(" _|");
         char c = '\u0041';
-        for(int i=1;i<=dim;i++){
-            System.out.print("_"+c+"__|");
+        for (int i = 1; i <= dim; i++) {
+            System.out.print("_" + c + "__|");
             c++;
         }
         System.out.print("_");
         System.out.print("\n");
-        for(int i=1; i<=dim; i++) {
-            System.out.print("|"+i+"|");
-            for(int j=1; j<=dim; j++){
+        for (int i = 1; i <= dim; i++) {
+            System.out.print("|" + i + "|");
+            for (int j = 1; j <= dim; j++) {
                 System.out.print("    |");
             }
-            System.out.print(i+"|");
+            System.out.print(i + "|");
             System.out.print("\n");
             System.out.print("|_|");
-            for(int j=1; j<=dim; j++){
+            for (int j = 1; j <= dim; j++) {
                 System.out.print("____|");
             }
             System.out.print("_|");
@@ -39,8 +44,8 @@ public class Output {
         }
         System.out.print("  |");
         c = '\u0041';
-        for(int i=1;i<=dim;i++){
-            System.out.print("_"+c+"__|");
+        for (int i = 1; i <= dim; i++) {
+            System.out.print("_" + c + "__|");
             c++;
         }
     }
