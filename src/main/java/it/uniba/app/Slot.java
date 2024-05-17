d6bc54dba6e3fc4125c9170f5e7615561b193cbc
B4RTM0SS <potitoflorio@outlook.it>
2024-05-17 17:53:27 +0200

    Aggiunto il javadoc relativo al costruttore privo di parametri della classe Slot e il colore VERDE per denotare una casella libera nell'enumerazione Color

--- src/main/java/it/uniba/app/Slot.java
+++ src/main/java/it/uniba/app/Slot.java
@@ -10,12 +10,17 @@ public class Slot {
      */
     private Color colorState;
 
+    /**
+     * Costruttore della classe slot che inizializza una casella libera, indicata con il colore
+     * <code>Color.VERDE</code>.
+     */
     Slot() {
         colorState = Color.VERDE;
     }
 
     /**
-     * Costruttore della classe slot.
+     * Costruttore della classe slot che inizializza il colore della stessa con quello passato
+     * come parametro attuale.
      * @param c il colore della casella.
      */
     public Slot(final Color c) {
