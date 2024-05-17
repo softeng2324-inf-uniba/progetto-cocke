a085ac6f5db0d71c26443de550f68e74cc486758
ninofrasca <162120477+ninofrasca@users.noreply.github.com>
2024-05-17 09:39:51 +0200

    Redesign da completare del metodo printEmptyField della classe output

--- src/main/java/it/uniba/app/Output.java
+++ src/main/java/it/uniba/app/Output.java
@@ -9,44 +9,38 @@ public final class Output {
     /**
      * Costruttore per la classe.
      */
-    private Output() { }
+    private Output() {
+    }
     /**
      * Metodo che mostra a video il campo vuoto su cui giocare.
      */
     static void printEmptyField() {
         final int dim = 7;
-        System.out.print("   ");
-        for (int i = 1; i <= dim; i++) {
-            System.out.print("____ ");
-        }
-        System.out.print("\n");
-        System.out.print(" _|");
+        System.out.print("   " + "\u200a" + "\u007c");
         char c = '\u0041';
-        for (int i = 1; i <= dim; i++) {
-            System.out.print("_" + c + "__|");
+        for (int i = 0; i < dim; i++) {
+            System.out.print("  " + c + "  " + "\u007c");
             c++;
         }
-        System.out.print("_");
         System.out.print("\n");
-        for (int i = 1; i <= dim; i++) {
-            System.out.print("|" + i + "|");
-            for (int j = 1; j <= dim; j++) {
-                System.out.print("    |");
-            }
-            System.out.print(i + "|");
-            System.out.print("\n");
-            System.out.print("|_|");
-            for (int j = 1; j <= dim; j++) {
-                System.out.print("____|");
-            }
-            System.out.print("_|");
-            System.out.print("\n");
+        System.out.print("   ");
+        System.out.print("\u2016");
+        System.out.print("\u200a");
+        for (int i = 0; i < dim - 1; i++) {
+            System.out.print("\u033f");
+            System.out.print("\u033f");
+            System.out.print("\u033f");
+            System.out.print("\u033f");
+            System.out.print("\u033f");
+            System.out.print("\u007c");
         }
-        System.out.print("  |");
-        c = '\u0041';
-        for (int i = 1; i <= dim; i++) {
-            System.out.print("_" + c + "__|");
-            c++;
-        }
+        System.out.print("\u033f");
+        System.out.print("\u033f");
+        System.out.print("\u033f");
+        System.out.print("\u033f");
+        System.out.print("\u033f");
+        System.out.print("\u200a");
+        System.out.print("\u2016");
+        System.out.print("\n");
     }
 }
