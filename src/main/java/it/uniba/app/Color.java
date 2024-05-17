693a4852ffd6680c353ecd5ecb8b41cb6f008ae4
frankrep <129170114+frankrep@users.noreply.github.com>
2024-05-17 18:48:02 +0200

    Rivisitata enumerazione Color
    L'enumerazione Color è stata trasformata in una enumerazione come classe, permettendo di conservare il codice del colore in codifica ad 8 bit.

--- src/main/java/it/uniba/app/Color.java
+++ src/main/java/it/uniba/app/Color.java
@@ -1,15 +1,36 @@
 package it.uniba.app;
 
 /**
- * Color è una enumerazione per la gestione dei colori. In questa enumerazione sono riportati tutti i colori utilizzati nel programma.
+ * Color è una enumerazione per la gestione dei colori.
+ * In questa enumerazione sono riportati tutti i colori utilizzati nel programma con la rispettiva codifica in 8 bit.
  */
 public enum Color {
-    ROSSO,
-    BLU,
-    BIANCO,
-    NERO,
-    ARANCIONE,
-    ROSA,
-    GIALLO
+    ROSSO(0),
+    BLU(0),
+    BIANCO(0),
+    NERO(0),
+    ARANCIONE(0),
+    ROSA(0),
+    GIALLO(0);
 
+    /**
+     * Codice del colore. La codifica utilizzata è a 8 bit.
+     */
+    private final int colorValue;
+
+    /**
+     * Imposta il codice del colore a quello specificato.
+     * @param value il valore del colore.
+     */
+    Color(final int value) {
+        colorValue = value;
+    }
+
+    /**
+     * Restituisce il codice del colore.
+     * @return il codice del colore.
+     */
+    public int getColorValue() {
+        return colorValue;
+    }
 }
