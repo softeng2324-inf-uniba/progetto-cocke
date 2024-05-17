2dd24bfd3efc58c5e1df89d1f87cd11f58044984
ninofrasca <162120477+ninofrasca@users.noreply.github.com>
2024-05-17 16:13:19 +0200

    Modificati i costruttori della classe Field come richiesto

--- src/main/java/it/uniba/app/Field.java
+++ src/main/java/it/uniba/app/Field.java
@@ -9,12 +9,16 @@ public class Field {
      * Matrice di slot che rappresenta il campo di gioco.
      */
     private Slot[][] gameboard;
-
+    static final int DEFAULT_DIM = 7;
     /**
      * Costruisce un oggetto Field
      */
     Field () {
-        gameboard = new Slot [7][7];
+        gameboard = new Slot [DEFAULT_DIM][DEFAULT_DIM];
+        gameboard[0][0].setColorState(Color.ROSSO);
+        gameboard[DEFAULT_DIM-1][DEFAULT_DIM-1].setColorState(Color.ROSSO);
+        gameboard[0][DEFAULT_DIM-1].setColorState(Color.BLU);
+        gameboard[DEFAULT_DIM-1][0].setColorState(Color.BLU);
     }
 
     /**
@@ -23,6 +27,10 @@ public class Field {
      */
     Field (int dim) {
         gameboard = new Slot [dim][dim];
+        gameboard[0][0].setColorState(Color.ROSSO);
+        gameboard[dim-1][dim-1].setColorState(Color.ROSSO);
+        gameboard[0][dim-1].setColorState(Color.BLU);
+        gameboard[dim-1][0].setColorState(Color.BLU);
     }
 
     /**
