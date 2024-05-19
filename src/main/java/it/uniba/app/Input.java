d1413cead580303670a90eb01b54febb455bc0af
ninofrasca <162120477+ninofrasca@users.noreply.github.com>
2024-05-19 12:33:57 +0200

    Sistemato checkstyle.

--- src/main/java/it/uniba/app/Input.java
+++ src/main/java/it/uniba/app/Input.java
@@ -1,37 +1,35 @@
 package it.uniba.app;
 
-import java.util.Scanner;
-
 /**
  * Classe Input utile all'acquisizione degli input da tastiera.
  */
 public final class Input {
     /**
-     * Costruttore per la classe Input
+     * Costruttore per la classe Input.
      */
-    private Input(){}
+    private Input() { }
 
     /**
-     * Metodo per l'acquisizione del nome di un giocatore
-     * @param playerIndex id del nuovo giocatore
-     * @return il nuovo oggetto giocatore
+     * Metodo per l'acquisizione del nome di un giocatore.
+     * @param playerIndex id del nuovo giocatore.
+     * @return il nuovo oggetto giocatore.
      */
-    public static Player getPlayersName(final int playerIndex){
+    public static Player getPlayersName(final int playerIndex) {
         Player p = null;
-        System.out.println("Enter player " + playerIndex+1 + " name");
-        if(playerIndex==1){
-            p = new Player(Color.BIANCO, Keyboard.readString());
+        System.out.println("Enter player " + playerIndex + 1 + " name");
+        if (playerIndex == 1) {
+            p = new Player(Color.WHITE, Keyboard.readString());
         } else {
-            p = new Player(Color.NERO, Keyboard.readString());
+            p = new Player(Color.BLACK, Keyboard.readString());
         }
         return p;
     }
 
     /**
-     * Metodo per l'acquisizione di una mossa da effettuare
-     * @return la mossa letta da tastiera
+     * Metodo per l'acquisizione di una mossa da effettuare.
+     * @return la mossa letta da tastiera.
      */
-    public static Move getMove(){
+    public static Move getMove() {
         char row = ' ';
         int col = -1;
         System.out.println("Enter the row of the draught to move");
@@ -48,10 +46,10 @@ public final class Input {
     }
 
     /**
-     * Metodo per l'acquisizione di una coordinata
-     * @return le coordinate lette da tastiera
+     * Metodo per l'acquisizione di una coordinata.
+     * @return le coordinate lette da tastiera.
      */
-    private static Coordinate getCoordinate(){
+    private static Coordinate getCoordinate() {
         char row = ' ';
         int col = -1;
         Coordinate c = null;
@@ -63,10 +61,10 @@ public final class Input {
     }
 
     /**
-     * Metodo per l'acquisizione di un comando
-     * @return il comando letto da tastiera
+     * Metodo per l'acquisizione di un comando.
+     * @return il comando letto da tastiera.
      */
-    public static String getCommand(){
+    public static String getCommand() {
         System.out.println("Enter a command");
         return Keyboard.readString();
     }
