cb40d10fd96acd929d2b12d7d028fe4ef17bf582
zrlvx <zeltrravx@gmail.com>
2024-05-20 10:28:15 +0200

    Aggiunta enumerazione per la gestione dei messaggi

---
+++ src/main/java/it/uniba/app/utils/Messages.java
@@ -0,0 +1,15 @@
+package it.uniba.app.utils;
+
+/**
+ * Enumerazione per gestire i messaggi da stampare a schermo.
+ */
+public enum Messages {
+    PARTITA_NON_AVVIATA,
+    CONFERMA_USCITA,
+    FLAG_NON_RICONOSCIUTA,
+    INSERIRE_NOME_GIOCATORE,
+    INSERIRE_COMANDO,
+    INSERIRE_RIGA,
+    INSERIRE_COLONNA,
+    COMANDO_SCONOSCIUTO,
+}
