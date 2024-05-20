269562fe6c5e8fb135e0cb0d50c5871799abe9a0
lorenzoappice <l.appice@studenti.uniba.it>
2024-05-20 13:16:43 +0200

    aggiunta java doc ad ogni package

---
+++ src/main/java/it/uniba/app/controller/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package contenente la classe GameController per il controllo del gioco.
+ */
+package it.uniba.app.controller;
---
+++ src/main/java/it/uniba/app/views/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Questo package contiene le classi che rappresentano le interfacce con l'utente dell'applicazione.
+ */
+package it.uniba.app.views;
