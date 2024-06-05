783804280125b735a6c85f3ab4cd053180da0de0
B4RTM0SS <potitoflorio@outlook.it>
2024-06-05 22:19:23 +0200

    Rimosso il commento segnalato e modificato il nome del parametro formale indicato.

--- src/main/java/it/uniba/app/model/Game.java
+++ src/main/java/it/uniba/app/model/Game.java
@@ -155,20 +155,19 @@ public class Game {
         return count;
     }
 
-    //Da valutare se inserire il colore come parametro così sia attua lo spostamento che la cattura
     /**
      * Modifica lo stato delle caselle circostanti e adiacenti alla casella presente nella posizione indicata
      * dalla coordinata in ingresso, eguagliandone il colore.
-     * @param coordinata posizione, sul campo da gioco, della casella da cui parte la cattura delle circostanti
+     * @param coordinate posizione, sul campo da gioco, della casella da cui parte la cattura delle circostanti
      */
-    void captureSlot(final Coordinate coordinata) {
-        Coordinate tempCoordinate = coordinata;
+    void captureSlot(final Coordinate coordinate) {
+        Coordinate tempCoordinate = coordinate;
         Field gameFieldCopy = getGameField();
-        Color colorSlot = gameFieldCopy.getSlot(coordinata).getColorState();
+        Color colorSlot = gameFieldCopy.getSlot(coordinate).getColorState();
         for (int rowOffset = -1; rowOffset < 2; rowOffset++) {
-            tempCoordinate.setRow(coordinata.getRow() + rowOffset);
+            tempCoordinate.setRow(coordinate.getRow() + rowOffset);
             for (int columnOffset = -1; columnOffset < 2; columnOffset++) {
-                tempCoordinate.setColumn(coordinata.getColumn() + columnOffset);
+                tempCoordinate.setColumn(coordinate.getColumn() + columnOffset);
                 gameFieldCopy.getSlot(tempCoordinate).setColorState(colorSlot);
             }
         }
