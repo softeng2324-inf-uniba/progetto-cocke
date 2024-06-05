579d6bb7769952b418ca3d4cb69bdc2956cb1219
vincxxdev <vincxxdev@gmail.com>
2024-06-05 05:56:11 +0200

    Aggiunto metodo per gestire le prossime mosse inserite dall'utente

--- src/main/java/it/uniba/app/views/Input.java
+++ src/main/java/it/uniba/app/views/Input.java
@@ -1,6 +1,4 @@
 package it.uniba.app.views;
-import it.uniba.app.model.Coordinate;
-import it.uniba.app.model.Move;
 import it.uniba.app.utils.Color;
 import it.uniba.app.model.Player;
 import it.uniba.app.utils.Message;
@@ -32,12 +30,78 @@ public final class Input {
     }
 
     /**
+     * Controlla l'indice della colonna e lo trasforma in un intero.
+     * @param row la riga da controllare.
+     * @return
+     */
+    private static int checkColsWord(final String row) {
+        switch (row.charAt(0)) {
+            case 'a':
+                return 0;
+            case 'b':
+                return 1;
+            case 'c':
+                return 2;
+            case 'd':
+                return 3;
+            case 'e':
+                return 4;
+            case 'f':
+                return 5;
+            case 'g':
+                return 6;
+            case 'h':
+                return 7;
+            default:
+                return -1;
+        }
+    }
+
+    /**
+     * Controlla l'indice della riga.
+     * @param col la colonna da controllare.
+     */
+    private static int checkRowsWord(final String col) {
+        switch (col.charAt(0)) {
+            case '1':
+                return 0;
+            case '2':
+                return 1;
+            case '3':
+                return 2;
+            case '4':
+                return 3;
+            case '5':
+                return 4;
+            case '6':
+                return 5;
+            case '7':
+                return 6;
+            case '8':
+                return 7;
+            default:
+                return -1;
+        }
+    }
+
+    /**
      * Metodo per l'acquisizione della prossima mossa dove il pattern è casella di partenza - casella di arrivo.
      */
-    public static String[] getNextMove() {
-        String nextMove = Keyboard.readString();
-        String[] nextMoveArray = nextMove.split("-");
-        return nextMoveArray;
+    public static String[] getNextMove(String command) {
+        if (command.length() == 5) {
+            String[] nextMoveArray = command.split("-");
+            if (nextMoveArray.length == 2)  {
+                int startRow = checkRowsWord(nextMoveArray[0].substring(1));
+                int startCol = checkColsWord(nextMoveArray[0].substring(0, 1));
+                int destRow = checkRowsWord(nextMoveArray[1].substring(1));
+                int destCol = checkColsWord(nextMoveArray[1].substring(0, 1));
+                if (startRow != -1 || startCol != -1 || destRow != -1 || destCol != -1) {
+                    return new String[]{nextMoveArray[0], nextMoveArray[1]};
+                }
+            }
+        }
+        Output.printMessages(Message.ILLEGAL_MOVE);
+        return null;
     }
 
     /**
