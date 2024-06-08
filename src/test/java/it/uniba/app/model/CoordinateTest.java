4c8826868fa40d30c236386db01dae07d926cb42
frankrep <129170114+frankrep@users.noreply.github.com>
2024-06-08 18:20:45 +0200

    Aggiunti casi di test alla classe
    I casi di test per le classi Coordinate e Field sono stati aggiunti

--- src/test/java/it/uniba/app/model/CoordinateTest.java
+++ src/test/java/it/uniba/app/model/CoordinateTest.java
@@ -1,5 +1,59 @@
 package it.uniba.app.model;
 
-public class CoordinateTest {
-    
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoordinateTest {
+
+    Coordinate tempCoordinate;
+
+    @BeforeEach
+    void setUp() {
+        tempCoordinate = new Coordinate(5, 6);
+    }
+
+    @Test
+    void testSetRow() {
+        tempCoordinate.setRow(10);
+        assertEquals(10, tempCoordinate.getRow(), "La riga non è stata modificata correttamente");
+    }
+
+    @Test
+    void testGetRow() {
+        assertEquals(5, tempCoordinate.getRow(), "La riga restituita non coincide con quella attesa");
+    }
+
+    @Test
+    void testSetColumn() {
+        tempCoordinate.setColumn(10);
+        assertEquals(10, tempCoordinate.getColumn(), "La colonna non è stata modificata correttamente");
+    }
+
+    @Test
+    void testGetColumn() {
+        assertEquals(6, tempCoordinate.getColumn(), "La colonna restituita non coincide con quella attesa");
+    }
+
+    @Test
+    void testCopyConstructor() {
+        Coordinate copy = new Coordinate(tempCoordinate);
+        assertAll("Costruttore di copia",
+                () -> assertEquals(5, copy.getRow(), "La riga restituita non coincide con quella attesa"),
+                () -> assertEquals(6, copy.getColumn(), "La colonna restituita non coincide con quella attesa")
+        );
+    }
+
+    @Test
+    void testEqualsCoordinate() {
+        Coordinate equalCoordinate = new Coordinate(5, 6);
+        assertTrue(equalCoordinate.equalsCoordinate(tempCoordinate), "Le coordinate non risultano uguali");
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("g6", tempCoordinate.toString(), "La stampa della coordinata non è corretta");
+    }
+
 }
--- src/test/java/it/uniba/app/model/FieldTest.java
+++ src/test/java/it/uniba/app/model/FieldTest.java
@@ -1,4 +1,62 @@
 package it.uniba.app.model;
 
+import it.uniba.app.utils.Color;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FieldTest {
+
+    Field tempField;
+    Slot tempSlot;
+
+    @BeforeEach
+    void setUp() {
+        tempField = new Field();
+        tempSlot = new Slot(Color.GOLD);
+        tempField.setSlot(new Coordinate(5, 5), tempSlot);
+    }
+
+    @Test
+    void testCopyConstructor() {
+        Field copy = new Field(tempField);
+        assertAll("Costruttore di copia",
+                () -> assertEquals(Color.GOLD, copy.getSlot(new Coordinate(5, 5)).getColorState()),
+                () -> assertEquals(Color.GREY, copy.getSlot(new Coordinate(4, 6)).getColorState())
+        );
+    }
+
+    @Test
+    void testLengthConstructor() {
+        Field copy = new Field(15);
+        assertAll("Costruttore con lunghezza campo",
+                () -> assertEquals(Color.GREY, copy.getSlot(new Coordinate(14, 14)).getColorState()),
+                () -> assertEquals(15, copy.length())
+        );
+    }
+
+    @Test
+    void testSetSlot() {
+        tempField.setSlot(new Coordinate(3, 3), tempSlot);
+        assertEquals(tempSlot.getColorState(), tempField.getSlot(new Coordinate(3, 3)).getColorState(), "Lo slot non è stato modificato correttamente");
+    }
+
+    @Test
+    void testGetSlot() {
+        assertAll("Getter di uno slot del campo",
+                () -> assertEquals(Color.GOLD, tempField.getSlot(new Coordinate(5, 5)).getColorState(), "Lo slot restituito non coincide con quello atteso"),
+                () -> assertEquals(Color.GREY, tempField.getSlot(new Coordinate(1, 2)).getColorState(), "Lo slot restituito non coincide con quello atteso")
+        );
+    }
+
+    @Test
+    void testLength() {
+        assertAll("Lunghezza di un campo",
+                () -> assertEquals(Field.DEFAULT_DIM, tempField.length(), "La dimensione restituita non coincide con quella attesa"),
+                () -> assertEquals(13, new Field(13).length(), "La dimensione restituita non coincide con quella attesa")
+        );
+    }
+
 }
