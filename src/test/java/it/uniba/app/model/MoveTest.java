780a038adffd71383a16fc6279c64de2b24e0386
B4RTM0SS <potitoflorio@outlook.it>
2024-06-08 22:03:05 +0200

    Completata la scrittura dei casi di test della classe Move.

--- src/test/java/it/uniba/app/model/MoveTest.java
+++ src/test/java/it/uniba/app/model/MoveTest.java
@@ -1,4 +1,39 @@
 package it.uniba.app.model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MoveTest {
+
+    private final Coordinate starting = new Coordinate(0,0);
+    private final Coordinate destination = new Coordinate(1,1);
+    private Move testMove;
+
+    @BeforeEach
+    void setUp() {
+        testMove = new Move(starting, destination);
+    }
+
+    @Test
+    void testGetStartingSlot() {
+        assertEquals(starting, testMove.getStartingSlot());
+    }
+
+    @Test
+    void testGetChosenSlot() {
+        assertEquals(destination, testMove.getChosenSlot());
+    }
+
+    @Test
+    void testGetDistance() {
+        assertEquals(1, testMove.getDistance());
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("a1-b2", testMove.toString());
+    }
+
 }
