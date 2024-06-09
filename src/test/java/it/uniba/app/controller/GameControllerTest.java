9dae9e6f6fc594942e179719e1063053753162e7
vincxxdev <vincxxdev@gmail.com>
2024-06-09 11:20:14 +0200

    Rimossi test di metodi privati

--- src/test/java/it/uniba/app/controller/GameControllerTest.java
+++ src/test/java/it/uniba/app/controller/GameControllerTest.java
@@ -1,7 +1,5 @@
 package it.uniba.app.controller;
 import it.uniba.app.model.*;
-import it.uniba.app.utils.Color;
-import it.uniba.app.views.Output;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,8 +12,6 @@ public class GameControllerTest {
 
     @BeforeEach
     public void before() {
-        Game testGame = new Game();
-        Game testOutput = new Game();
         controller = new GameController();
     }
 
@@ -29,15 +25,12 @@ public class GameControllerTest {
     @Test
     void testSetGame() {
         Game newGame = new Game();
-        //Game copiedGame = new Game(newGame);
         controller.setGame(newGame);
         assertNotNull(controller.getGame());
-        //assertEquals(copiedGame, controller.getGame());
     }
 
     @Test
     void testGetStillPlaying() {
-        // Valore iniziale di stillPlaying dovrebbe essere true
         assertTrue(controller.getStillPlaying());
     }
 
@@ -50,60 +43,6 @@ public class GameControllerTest {
         // Imposta stillPlaying a true e verifica
         controller.setStillPlaying(true);
         assertTrue(controller.getStillPlaying());
-    }
-
-    @Test
-    public void testStartGameIsNotNull() {
-        controller.startNewGame();
-        assertNotNull(controller.getGame());
-    }
-
-    @Test
-    public void testConvertField() {
-        int fieldSize = 7;
-        Field field = new Field(fieldSize);
-        Color playerColor = Color.WHITE;
-//inserisce 2 pedine in a1 e a2 , cancellare questo commento dopo la supervisione
-        field.getSlot(new Coordinate(1, 1)).setColorState(playerColor);
-        field.getSlot(new Coordinate(1, 2)).setColorState(playerColor);
-
-
-        GameController controller = new GameController();
-        controller.convertField(field, playerColor);
-
-//verifica che ci sia il colore giallo in 2,1 arancione 3,1 e rosa 2,3. da finire di implementare
-        assertEquals(playerColor, field.getSlot(new Coordinate(2, 1)).getColorState());
-        assertEquals(playerColor, field.getSlot(new Coordinate(3, 1)).getColorState());
-        assertEquals(playerColor, field.getSlot(new Coordinate(2, 3)).getColorState());
-    }
-
-    @Test //inserisce tutti pezzi neri e restituisce falso con i colori rosa, arancione e giallo
-    public void testConvertFullField() {
-        int fieldSize = 7;
-        Field field = new Field(fieldSize);
-        Color playerColor = Color.BLACK;
-
-        for (int x = 0; x < fieldSize; x++) {
-            for (int y = 0; y < fieldSize; y++) {
-                Coordinate coordinate = new Coordinate(x, y);
-                field.getSlot(coordinate).setColorState(playerColor);
-            }
-        }
-
-        assertEquals(playerColor, field.getSlot(new Coordinate(4, 3)).getColorState());
-    }
-
-
-    @Test
-    public void testCheckDistanceReturnsTrueWithinRange() {
-        int distanceWithinRange = 2;
-        assertTrue(controller.checkDistance(distanceWithinRange));
-    }
-
-    @Test
-    public void testCheckDistanceReturnsFalseOutOfRange() {
-        int distanceOutOfRange = 15;
-        assertFalse(controller.checkDistance(distanceOutOfRange));
     }
 
     @Test
