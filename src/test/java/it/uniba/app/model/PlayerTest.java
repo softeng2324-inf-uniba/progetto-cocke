c642e14dead3e01ffc366c60a676e694d9de174f
B4RTM0SS <potitoflorio@outlook.it>
2024-06-09 17:47:49 +0200

    Sistemati errori CheckStyle nella classe PlayerTest.

--- src/test/java/it/uniba/app/model/PlayerTest.java
+++ src/test/java/it/uniba/app/model/PlayerTest.java
@@ -8,21 +8,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class PlayerTest {
 
     /**
-     * <Code>Player</Code> test utilizzato all'interno dei test della classe, inizializzato con il colore <Code>Color.BLACK</Code>
-     * e il nome "Giocatore 1".
+     * <Code>Player</Code> test utilizzato all'interno dei test della classe, inizializzato con il colore
+     * <Code>Color.BLACK</Code> e il nome "Giocatore 1".
      */
     private final Player testPlayer = new Player(Color.BLACK, "Giocatore 1");
 
     /**
-     * Messaggio d'errore nel caso in cui la copia eseguita attraverso il costruttore di copia della classe <code>Player</code>,
-     * non corrisponda all'istanza di <code>Player</code> in ingresso al costruttore stesso.
+     * Messaggio d'errore nel caso in cui la copia eseguita attraverso il costruttore di copia della classe
+     * <code>Player</code>, non corrisponda all'istanza di <code>Player</code> in ingresso al costruttore stesso.
      */
-    private final static String WRONG_COPY = "La copia del giocatore eseguita non corrisponde al giocatore copiato.";
+    private static final String WRONG_COPY = "La copia del giocatore eseguita non corrisponde al giocatore copiato.";
 
     /**
      * Messaggio d'errore nel caso in cui il colore del <code>Player</code> in esame non corrisponda a quello previsto.
      */
-    private final static String WRONG_COLOR = "Il colore del giocatore non corrisponde a quello atteso.";
+    private static final String WRONG_COLOR = "Il colore del giocatore non corrisponde a quello atteso.";
 
     /**
      * Messaggio d'errore nel caso in cui il nome del <code>Player</code> in esame non corrisponda al nome atteso.
@@ -40,10 +40,15 @@ class PlayerTest {
      */
     private static final String WRONG_HASH = "Il valore hash del giocatore calcolato non corrisponde a quello atteso.";
 
+    /**
+     * Valore hash di testPlayer con colore <code>Color.BLACK</code> e nome "Giocatore 1".
+     */
+    private static final int HASH_BLACK_PLAYER_ONE = -628335851;
+
 
     /**
-     * Test del costruttore di copia della classe <Code>Player</Code>, per cui viene inizializzato un nuovo giocatore con
-     * gli stessi valori degli attributi contenuti all'interno di testPlayer, e su questi viene eseguito il test.
+     * Test del costruttore di copia della classe <Code>Player</Code>, per cui viene inizializzato un nuovo giocatore
+     * con gli stessi valori degli attributi contenuti all'interno di testPlayer, e su questi viene eseguito il test.
      */
     @Test
     void testCopyConstructor() {
@@ -85,7 +90,7 @@ class PlayerTest {
      */
     @Test
     void testHashCode() {
-        assertEquals(-628335851, testPlayer.hashCode(), WRONG_HASH);
+        assertEquals(HASH_BLACK_PLAYER_ONE, testPlayer.hashCode(), WRONG_HASH);
     }
 
 }
